d506306f46934805b8f91985ecca62f19ff13c38
Mayur541 <mayurshahapurkar5412@gmail.com>
2025-12-06 20:15:36 +0530

    Initial Project Setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('Clean Quest Backend is Running!');
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
