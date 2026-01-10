82786f0168cba01139f54d70d19fe1ac28998eee
Mayur541 <mayurshahapurkar5412@gmail.com>
2026-01-11 01:30:54 +0530

    fix

--- CleanQuest/server/models/Admin.js
+++
@@ -1,9 +0,0 @@
-// server/models/Admin.js
-const mongoose = require('mongoose');
-
-const AdminSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true } // This will be the "Hashed" password
-});
-
-module.exports = mongoose.model('Admin', AdminSchema);
